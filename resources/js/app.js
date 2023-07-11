9f9f9c2b0c9ccd0012a4d33a0df72350cea3c948
Tanya <voznyuk1998@mail.ru>
2023-07-11 13:49:57 +0200

    react,css updated

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,13 +3,13 @@
  * includes React and other helpers. It's a great starting point while
  * building robust, powerful web applications using React + Laravel.
  */
-import './bootstrap';
+
+require('./bootstrap');
+
 /**
  * Next, we will create a fresh React component instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-//import Demo from './components/Demo';
-import Demo from 'js/components/Demo';
-import './components/Example';
+require('./components/Example');
