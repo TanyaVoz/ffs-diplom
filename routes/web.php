20f0b1a5159931a83b56af91766a5766380888bf
Tanya <voznyuk1998@mail.ru>
2023-07-12 16:54:32 +0200

    css,admin updated

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Route::get('/admin', function () {
+    return view('admin');
+});
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .react()
-    .sass('resources/sass/app.scss', 'public/css');
+    .css('resources/css/normalize.css', 'public/css')
+    .sass('resources/sass/styles.scss', 'public/css')
+    .css('resources/css/admin/normalize.css', 'public/css/admin')
+    .sass('resources/sass/admin/styles.scss', 'public/css/admin')
